6c99c83465802f954977a190503bde69fda2c4b9
Adam Duchna <adamduchna43@gmail.com>
2021-05-05 11:30:52 +0200

    lab10

---
+++ lab10/react-lab/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum felis sit amet dui pharetra."
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab10/react-lab/src/Components.js
@@ -0,0 +1,10 @@
+ function About(){
+    return (
+       const tresc=( <h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus, nibh.
+        </h1> )
+    )
+}
+export default About;
+
+
